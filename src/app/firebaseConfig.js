8f5902915cd983c974902a7201cd8714974f98a2
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-10-19 23:42:25 +0530

    shit it works

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, push, set } from "firebase/database";
 
 const firebaseConfig = {
@@ -15,7 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
 
 export { database };
