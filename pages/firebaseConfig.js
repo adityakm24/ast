12d04f235052f1f161ed6288d8831c2e23442bf4
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-10-19 23:36:07 +0530

    Update firebaseConfig.js

--- pages/firebaseConfig.js
+++ pages/firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, push, set } from "firebase/database";
 
 const firebaseConfig = {
